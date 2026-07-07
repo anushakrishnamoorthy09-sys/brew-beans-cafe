97892c945fd65d8676e905492c776982f5848ff9
Anusha Krishnamoorthy <anushakrishnamoorthy09@gmail.com>
2026-07-07 22:56:10 +0530

    checked the error

--- server/server.js
+++ server/server.js
@@ -13,6 +13,10 @@ app.use("/api/orders", orderRoutes);
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("Backend Working");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
