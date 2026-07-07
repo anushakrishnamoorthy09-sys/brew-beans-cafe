27d13051a0b14079dbedeeeba206d17a6e9ff948
Anusha Krishnamoorthy <anushakrishnamoorthy09@gmail.com>
2026-07-07 23:17:19 +0530

    Add reservation and message routes

--- server/server.js
+++ server/server.js
@@ -5,17 +5,23 @@ require("dotenv").config();
 const app = express();
 
 const orderRoutes = require("./routes/orderRoutes");
+const reservationRoutes = require("./routes/reservationRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use("/api/orders", orderRoutes);
+app.use("/api/reservations", reservationRoutes);
+app.use("/api/messages", messageRoutes);
 
-const PORT = process.env.PORT || 5000;
-
+// Home Route
 app.get("/", (req, res) => {
   res.send("Backend Working");
 });
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
